cd26ef22816ec9dd18372b9b6eaf9b51366cbc01
younaship <dev@younaship.com>
2019-10-17 00:59:31 +0900

    update

---
+++ MonoShared/YPMono/Screen.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YPMono
+{
+    public class Screen
+    {
+        public int height { get { return GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height; } }
+        public int width { get { return GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width; } }
+    }
+}
---
+++ MonoShared/YPMono/YPGUI/ImageButton.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input.Touch;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YPMono.YPGUI
+{
+    public class ImageButton : UI
+    {
+        public GUIEventHundler onClick { set; get; }
+        public GUIEventHundler onHover { set; get; }
+
+        public Color DefaultColor { set; get; }
+        public Color HoverColor { set; get; }
+
+        public Texture2D texture { set; get; }
+
+        public ImageButton()
+        {
+            enableReciveTap = true;
+            DefaultColor = Color.White;
+            HoverColor = Color.DarkGray;
+            
+        }
+
+        public override void OnCreate(YPScene scene)
+        {
+            base.OnCreate(scene);
+        }
+
+        public override void Start(YPScene scene)
+        {
+            base.Start(scene);
+        }
+
+        public override void Update(YPScene scene)
+        {
+            base.Update(scene);
+
+            if (IsVisible)
+                scene.drawEvents += (x) =>
+                {
+                    if (IsPress) x.Draw(texture, new Rectangle(transform.Position.ToPoint(), transform.Size.ToPoint()), HoverColor);
+                    else x.Draw(texture, new Rectangle(transform.Position.ToPoint(), transform.Size.ToPoint()), DefaultColor);
+                };
+        }
+
+        /// <summary>
+        /// This act is before Update();
+        /// </summary>
+        protected override void OnClick()
+        {
+            base.OnClick();
+            onClick?.Invoke();
+        }
+
+        /// <summary>
+        /// This act is before Update();
+        /// </summary>
+        protected override void OnHover()
+        {
+            base.OnHover();
+            onHover?.Invoke();
+        }
+    }
+    
+}
